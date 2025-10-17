a7e3bbd0fc84b9a324eec18900cd4ca472b6076b
Karleigh Branscomb <kbranscomb@ualr.edu>
2025-10-17 23:33:03 +0000

    class notes finish

--- classes-inheritance/personal-class-example.cpp
+++ classes-inheritance/personal-class-example.cpp
@@ -25,6 +25,25 @@ class Fraction{
 
 };
 
+
+struct Base{
+    void speak()const{ std::cout << "woof" << "\n"; }
+    /*
+    a function that returns nothing has no parameters.; Modifies the object. 
+    */
+};
+
+struct Dervied : public Base{}; //as-is relationship
+/*
+    defines new struct and inherits it to base.
+*/
+
+/*
+    Protected is accessible in derived
+    for data intended for derived classes. 
+*/
+
+
 int Fraction::instances = 0;
 /*
 need this because: 
@@ -33,13 +52,65 @@ need this because:
             Define it separately (outside class or another file) --> Linker knows to allocate memory for it. 
 */
 
+
+// struct B{};
+
+// struct D : B{ //Initializes B first the D
+
+//     D() : B(), a(1), b(a){} //Constructor with Initializer
+
+//     //Data members
+//     int a; 
+//     int b; 
+
+//     /*
+//         if -->  int b; --> Initialized before we know what a is
+//                 int a; --> set to 1
+
+//             UNDEFINED BEHAVIOR
+//     */
+
+// };
+
+
+struct B{
+
+    virtual void draw()const; 
+    //allows function to be overridden
+};
+
+struct D : B{
+    void draw() const override; 
+    //overrides function in B
+};
+
+void render(const B& obj){
+    //reference to be and makes a instance named obj
+    //Calls what ever draw we need from either B or D.
+    /*
+        Since D derives from B, it will call which ever one we need. and render it. 
+    */
+    obj.draw();
+}
+
+
 int main(){
+
+    B b;
+    D d; 
+
+    render(b);
+    render(d); //calls d draw and not b draw because "d" is of D
+
+    Dervied d; 
+
+    // d.speak();
 
     Fraction a(1,2);
     Fraction b(3,4);
 
     std::cout << "a:" << a << "\n";
-    std::cout << "b: " << b << "\n";
+    // std::cout << "b: " << b << "\n";
 
     std::cout << "Instances: " << Fraction::instances << "\n";
 
